56a80997739bf2edd9088706d462662386839d9c
carlos123asd <carlos-medin@hotmail.com>
2024-05-01 17:28:43 +0200

    primera subida

---
+++ src/main/java/logica/ModelodeCapas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author EDALON
+ */
+public class ModelodeCapas {
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
